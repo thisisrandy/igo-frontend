2e2ed7dfbdc0bcaace11dded0234dbaa2f048200
Randy West <randywest55@gmail.com>
2021-04-12 18:05:55 -0700

    Add FIXME

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// FIXME: This is currently only set up to test the template App
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
