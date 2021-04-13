2a797f08ae839a71e8a1949c4eba172a48cc4581
Randy West <randywest55@gmail.com>
2021-04-12 18:08:56 -0700

    Reorganize into folders

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "../components/App";
 
 // FIXME: This is currently only set up to test the template App
 test("renders learn react link", () => {
