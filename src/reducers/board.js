30b1e6a7a2dfd1345ed0d99bea77d0542128ee66
Randy West <randywest55@gmail.com>
2021-04-13 21:48:40 -0700

    Add extra default pieces

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,8 +1,15 @@
 const pieces = Array.from({ length: 19 }, () =>
   Array.from({ length: 19 }, () => ".")
 );
+// TODO: Remove these after play logic is implemented
 pieces[5][5] = "w";
+pieces[5][6] = "w";
+pieces[6][6] = "w";
+pieces[6][5] = "b";
 pieces[8][10] = "b";
+pieces[9][10] = "b";
+pieces[9][11] = "b";
+pieces[8][11] = "w";
 const initialState = {
   pieces: pieces,
 };
