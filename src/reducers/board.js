cef4f070a7124e2f1d528b291d448acda749a9f2
Randy West <randywest55@gmail.com>
2021-04-15 15:59:33 -0700

    Remove placed stones from initial state

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,19 +1,9 @@
 import { PLACE_STONE } from "../constants/ActionTypes";
 
-const stones = Array.from({ length: 19 }, () =>
-  Array.from({ length: 19 }, () => ".")
-);
-// TODO: Remove these after play logic is implemented
-stones[5][5] = "w";
-stones[5][6] = "w";
-stones[6][6] = "w";
-stones[6][5] = "b";
-stones[8][10] = "b";
-stones[9][10] = "b";
-stones[9][11] = "b";
-stones[8][11] = "w";
 const initialState = {
-  stones: stones,
+  stones: Array.from({ length: 19 }, () =>
+    Array.from({ length: 19 }, () => ".")
+  ),
 };
 
 export default function board(state = initialState, action) {
