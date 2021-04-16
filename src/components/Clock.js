8fb7eb6f9411c60fec89d30560c8526e982a22da
Randy West <randywest55@gmail.com>
2021-04-16 14:13:50 -0700

    Add days display to clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -11,9 +11,11 @@ function Clock() {
     setTimeout(() => setTime(time + 1), 1000);
   });
 
-  // NOTE: This "overflows" after 24 hours, but I don't really care
   const hhmmss = (seconds) => {
-    return new Date(seconds * 1000).toISOString().slice(11, 19);
+    return (
+      (seconds >= 86400 ? Math.floor(seconds / 86400) + "d, " : "") +
+      new Date(seconds * 1000).toISOString().slice(11, 19)
+    );
   };
 
   return (
