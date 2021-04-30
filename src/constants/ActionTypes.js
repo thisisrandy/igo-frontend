62f05033eba73e0445fffacd7c853924404218a5
Randy West <randywest55@gmail.com>
2021-04-30 14:47:27 -0700

    Add redux-websocket constants

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,1 +1,11 @@
+import {
+  DEFAULT_PREFIX,
+  WEBSOCKET_CLOSED,
+  WEBSOCKET_MESSAGE,
+  WEBSOCKET_OPEN,
+} from "@giantmachines/redux-websocket";
+
 export const PLACE_STONE = "PLACE_STONE";
+export const WS_CLOSED = `${DEFAULT_PREFIX}::${WEBSOCKET_CLOSED}`;
+export const WS_OPEN = `${DEFAULT_PREFIX}::${WEBSOCKET_OPEN}`;
+export const WS_MESSAGE = `${DEFAULT_PREFIX}::${WEBSOCKET_MESSAGE}`;
