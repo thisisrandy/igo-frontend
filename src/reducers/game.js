1cc15bd2168b64702407e080e0a81d9a731e0bbc
Randy West <randywest55@gmail.com>
2021-04-30 14:49:30 -0700

    Add basic websocket action handling

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,9 +1,16 @@
-import { PLACE_STONE } from "../constants/ActionTypes";
+import {
+  PLACE_STONE,
+  WS_CLOSED,
+  WS_MESSAGE,
+  WS_OPEN,
+} from "../constants/ActionTypes";
+import { GAME_STATUS } from "../constants/IncomingMessageTypes";
 
 const initialState = {
   board: Array.from({ length: 19 }, () =>
     Array.from({ length: 19 }, () => ["", false, false, ""])
   ),
+  connected: false,
 };
 
 export default function board(state = initialState, action) {
@@ -22,6 +29,39 @@ export default function board(state = initialState, action) {
           )
         ),
       };
+    case WS_MESSAGE:
+      console.log("got message!");
+      const msg = action.payload.message;
+      console.log(msg.message_type);
+      // check the type and process accordingly
+      switch (action.payload.message.message_type) {
+        case GAME_STATUS:
+          console.log("got game status");
+          return {
+            ...state,
+            ...action.payload.message.data,
+          };
+        default:
+          return state;
+      }
+    case WS_OPEN:
+      // Check if we have a game open and rejoin if necessary
+      console.log("websocket open");
+      return {
+        ...state,
+        connected: true,
+      };
+    case WS_CLOSED:
+      // TODO: Pop up an alert saying we're disconnected
+      // Also, we might actually want WEBSOCKET_BROKEN, since we will never
+      // manually close the connection, but it might drop and need to be
+      // reconnected
+      console.log("websocket closed");
+      return {
+        ...state,
+        connected: false,
+      };
+
     default:
       return state;
   }
