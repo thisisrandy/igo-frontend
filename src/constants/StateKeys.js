18b9a80455f713ccf509b4f7e4e207ef34e407f6
Randy West <randywest55@gmail.com>
2021-04-30 16:02:30 -0700

    Add additional keys + organize

--- src/constants/StateKeys.js
+++ src/constants/StateKeys.js
@@ -1,6 +1,11 @@
 import { KEYS, YOUR_COLOR } from "./IncomingMessageKeys";
 
+// response container keys
+export const SUCCESS = "success";
+export const EXPLANATION = "explanation";
 export { KEYS, YOUR_COLOR };
+
+// game keys
 export const BOARD = "board";
 export const STATUS = "status";
 export const KOMI = "komi";
@@ -9,3 +14,7 @@ export const TURN = "turn";
 export const TERRITORY = "territory";
 export const PENDINGREQUEST = "pendingRequest";
 export const RESULT = "result";
+
+// internal keys
+export const ALERT = "alert";
+export const PERISTENT_ALERT = "peristent_alert";
