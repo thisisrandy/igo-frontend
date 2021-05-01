fd764fee6de9fb7d53ffde4b7a39c648ef0ec9f5
Randy West <randywest55@gmail.com>
2021-05-01 12:34:02 -0700

    Add TODO

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -5,6 +5,7 @@ import {
   WEBSOCKET_OPEN,
 } from "@giantmachines/redux-websocket";
 
+// TODO: get rid of place stone
 export const PLACE_STONE = "PLACE_STONE";
 export const WS_CLOSED = `${DEFAULT_PREFIX}::${WEBSOCKET_CLOSED}`;
 export const WS_OPEN = `${DEFAULT_PREFIX}::${WEBSOCKET_OPEN}`;
