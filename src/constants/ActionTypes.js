5f0dd5c3d19c5f893def48c7a1d4a0956e4d10d3
Randy West <randywest55@gmail.com>
2021-05-02 11:51:58 -0700

    Add clarifying comment

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -7,6 +7,8 @@ import {
 
 // TODO: get rid of place stone
 export const PLACE_STONE = "PLACE_STONE";
+
+// websocket actions
 export const WS_CLOSED = `${DEFAULT_PREFIX}::${WEBSOCKET_CLOSED}`;
 export const WS_OPEN = `${DEFAULT_PREFIX}::${WEBSOCKET_OPEN}`;
 export const WS_MESSAGE = `${DEFAULT_PREFIX}::${WEBSOCKET_MESSAGE}`;
