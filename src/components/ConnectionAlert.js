17579558c9f83c4ab98affd487842717a43a41f4
Randy West <randywest55@gmail.com>
2021-05-03 14:40:15 -0700

    Add delay to ConnectionAlert

--- src/components/ConnectionAlert.js
+++ src/components/ConnectionAlert.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PersistentAlert from "./PersistentAlert";
 import { CONNECTED } from "../constants/StateKeys";
 import { useSelector } from "react-redux";
@@ -8,10 +8,20 @@ import dedent from "dedent-js";
 function ConnectionAlert({ zIndex }) {
   const { [CONNECTED]: connected } = useSelector((state) => state.game);
 
+  // We don't want this popping up immediately before the initial connection
+  // under normal circumstances or otherwise for a short blip in the connection,
+  // so give it a small delay
+  const [hidden, setHidden] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setHidden(false);
+    }, 1000);
+  });
+
   return (
     <PersistentAlert
       zIndex={zIndex}
-      open={!connected}
+      open={!hidden && !connected}
       message={dedent(
         `Connection to the game server (${SERVER_URI}) either lost or not
         previously established. Attempting to (re)connect...`
