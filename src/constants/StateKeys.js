08a5a24796322f3bce5e946e43567a8c18b7236a
Randy West <randywest55@gmail.com>
2021-05-03 19:58:32 -0700

    Add OPPONENT_CONNECTED state key

--- src/constants/StateKeys.js
+++ src/constants/StateKeys.js
@@ -13,6 +13,7 @@ export const TURN = "turn";
 export const TERRITORY = "territory";
 export const PENDINGREQUEST = "pendingRequest";
 export const RESULT = "result";
+export const OPPONENT_CONNECTED = "opponentConnected";
 
 // message display keys
 export const MESSAGE = "message";
