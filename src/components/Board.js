a22ddd6d1860f32e3f35e89d24f3ed8332871303
Randy West <randywest55@gmail.com>
2021-05-06 13:30:45 -0700

    Add display support for multiple board sizes + TODO
    I didn't consider the need for borders large enough to fit stones, so
    the board images will need to be redone and the grid styling
    reconsidered accordingly

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useStyles } from "../hooks/useStyles";
-import goBoard from "../images/board_19x19.png";
+import board_9x9 from "../images/board_9x9.png";
+import board_13x13 from "../images/board_13x13.png";
+import board_19x19 from "../images/board_19x19.png";
 import { Paper } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import { BOARD } from "../constants/StateKeys";
@@ -13,6 +15,22 @@ function Board({ myTurn, playing, endGame }) {
     (state) => state.game[BOARD]
   );
 
+  let board = null;
+  switch (size) {
+    case 9:
+      board = board_9x9;
+      break;
+    case 13:
+      board = board_13x13;
+      break;
+    case 19:
+      board = board_19x19;
+      break;
+    default:
+      throw new RangeError(`Unsupported board size ${size}`);
+  }
+  // TODO: adjust this accordingly for 9x9 and 13x13 boards with appropriately
+  // sized borders
   const gridTemplateStyling = `0.25fr repeat(${size}, ${19 / size}fr) 0.25fr`;
 
   return (
@@ -23,7 +41,7 @@ function Board({ myTurn, playing, endGame }) {
         gridTemplateRows: gridTemplateStyling,
       }}
     >
-      <img src={goBoard} alt="go board" className={classes.BoardImage} />
+      <img src={board} alt="go board" className={classes.BoardImage} />
       {Array.from(points.entries(), ([i, row]) =>
         Array.from(row.entries(), ([j, point]) => (
           <Point
