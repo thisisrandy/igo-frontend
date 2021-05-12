60f16e76a31f3a64b38c91d12098d353c7e015ea
Randy West <randywest55@gmail.com>
2021-05-12 10:14:51 -0700

    Remove unused App test

--- src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../components/App";
-
-// FIXME: This is currently only set up to test the template App
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
