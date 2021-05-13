cbf4e1c9a7562aac78733b872aed1731e2242fce
Randy West <randywest55@gmail.com>
2021-05-12 19:29:08 -0700

    Add board size test

--- src/tests/Board.test.js
+++ src/tests/Board.test.js
@@ -68,8 +68,19 @@ test("renders Board with several stones", () => {
   expect(black.length).toBe(3);
 });
 
+test("board size is correct", () => {
+  render(<Board myTurn={true} playing={true} endGame={false} />, {
+    initialState: emptyBoard,
+  });
+  const bottomRight = screen.getByRole("button", { name: /\(18, 18\)/ });
+  expect(bottomRight).toBeInTheDocument();
+  const tooFarDown = screen.queryByRole("button", { name: /\(19, 18\)/ });
+  expect(tooFarDown).not.toBeInTheDocument();
+  const tooFarRight = screen.queryByRole("button", { name: /\(18, 19\)/ });
+  expect(tooFarRight).not.toBeInTheDocument();
+});
+
 // TODO: check correct handlers called/dispatches made on clicks
-// TODO: check correct board size used
 // TODO: check marked dead and counted work as expected
 // TODO: check hover works as expected
 // TODO: check stones placed in correct spot. this might be difficult to verify
