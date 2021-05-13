f4887f6d5e15e1ebbe6fc0f387366f5f95ea9ac1
Randy West <randywest55@gmail.com>
2021-05-13 13:23:58 -0700

    Add send action type

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -3,12 +3,14 @@ import {
   WEBSOCKET_CLOSED,
   WEBSOCKET_MESSAGE,
   WEBSOCKET_OPEN,
+  WEBSOCKET_SEND,
 } from "@giantmachines/redux-websocket";
 
 // websocket actions
 export const WS_CLOSED = `${DEFAULT_PREFIX}::${WEBSOCKET_CLOSED}`;
 export const WS_OPEN = `${DEFAULT_PREFIX}::${WEBSOCKET_OPEN}`;
 export const WS_MESSAGE = `${DEFAULT_PREFIX}::${WEBSOCKET_MESSAGE}`;
+export const WS_SEND = `${DEFAULT_PREFIX}::${WEBSOCKET_SEND}`;
 
 // internal actions
 export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
