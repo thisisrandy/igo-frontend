1871f8c980ca59a9b802c217739fd23eb7d5c0fb
Randy West <randywest55@gmail.com>
2021-05-13 18:21:01 -0700

    Fix clock test (still using deleted test-utils)

--- src/tests/Clock.test.js
+++ src/tests/Clock.test.js
@@ -1,27 +1,41 @@
-import { act, render, screen } from "../utils/test-utils";
+import { act, render, screen } from "@testing-library/react";
 import Clock from "../components/Clock";
 import { TIME_PLAYED, STATUS } from "../constants/StateKeys";
 import { COMPLETE, PLAY } from "../constants/GameStatus";
+import createMockStore from "redux-mock-store";
+import { Provider } from "react-redux";
+
+const mockStore = createMockStore([]);
 
 test("renders Clock", () => {
-  render(<Clock />, { initialState: { game: {} } });
+  render(
+    <Provider store={mockStore({ game: {} })}>
+      <Clock />
+    </Provider>
+  );
   const clock = screen.getByText("Time played: 00:00:00");
   expect(clock).toBeInTheDocument();
 });
 
 test("Clock uses server time", () => {
-  render(<Clock />, {
-    initialState: { game: { [TIME_PLAYED]: 92165, [STATUS]: PLAY } },
-  });
+  render(
+    <Provider
+      store={mockStore({ game: { [TIME_PLAYED]: 92165, [STATUS]: PLAY } })}
+    >
+      <Clock />
+    </Provider>
+  );
   const clock = screen.getByText("Time played: 1d, 01:36:05");
   expect(clock).toBeInTheDocument();
 });
 
 test("Clock ticks during play", () => {
   jest.useFakeTimers();
-  render(<Clock />, {
-    initialState: { game: { [TIME_PLAYED]: 0, [STATUS]: PLAY } },
-  });
+  render(
+    <Provider store={mockStore({ game: { [TIME_PLAYED]: 0, [STATUS]: PLAY } })}>
+      <Clock />
+    </Provider>
+  );
   act(() => {
     jest.advanceTimersByTime(66000);
   });
@@ -31,9 +45,13 @@ test("Clock ticks during play", () => {
 
 test("Clock doesn't tick after play complete", () => {
   jest.useFakeTimers();
-  render(<Clock />, {
-    initialState: { game: { [TIME_PLAYED]: 0, [STATUS]: COMPLETE } },
-  });
+  render(
+    <Provider
+      store={mockStore({ game: { [TIME_PLAYED]: 0, [STATUS]: COMPLETE } })}
+    >
+      <Clock />
+    </Provider>
+  );
   act(() => {
     jest.advanceTimersByTime(66000);
   });
