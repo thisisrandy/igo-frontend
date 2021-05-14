ae29614b34a5ef3506a0efa698606f23fbe58726
Randy West <randywest55@gmail.com>
2021-05-13 17:34:54 -0700

    Add ConnectionAlert test

---
+++ src/tests/ConnectionAlert.test.js
@@ -0,0 +1,40 @@
+import { render, act, screen } from "@testing-library/react";
+import createMockStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { CONNECTED } from "../constants/StateKeys";
+import ConnectionAlert from "../components/ConnectionAlert";
+import { CONNECTION_ALERT_DELAY_MS } from "../constants/ConnectionAlertDelay";
+
+const mockStore = createMockStore([]);
+const queryExpr = /Connection to the game server.*lost/;
+
+test("alert not present when connected", () => {
+  const store = mockStore({ game: { [CONNECTED]: true } });
+  render(
+    <Provider store={store}>
+      <ConnectionAlert zIndex={1} />
+    </Provider>
+  );
+
+  const alert = screen.queryByText(queryExpr);
+  expect(alert).not.toBeInTheDocument();
+});
+
+test("alert present when disconnected", () => {
+  jest.useFakeTimers();
+  const store = mockStore({ game: { [CONNECTED]: false } });
+  render(
+    <Provider store={store}>
+      <ConnectionAlert zIndex={1} />
+    </Provider>
+  );
+
+  // there is a delay built into the display, so make sure to account for it
+  let alert = screen.queryByText(queryExpr);
+  expect(alert).not.toBeInTheDocument();
+  act(() => {
+    jest.advanceTimersByTime(CONNECTION_ALERT_DELAY_MS);
+  });
+  alert = screen.queryByText(queryExpr);
+  expect(alert).toBeInTheDocument();
+});
