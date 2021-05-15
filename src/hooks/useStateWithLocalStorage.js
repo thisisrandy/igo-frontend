f8475bf217eb62305f798d9b1b74bc7ab92ca7f5
Randy West <randywest55@gmail.com>
2021-05-15 14:55:54 -0700

    Add useStateWithLocalStorage

---
+++ src/hooks/useStateWithLocalStorage.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export function useStateWithLocalStorage(
+  key,
+  initialValue,
+  filter = () => true
+) {
+  if (key in localStorage) initialValue = JSON.parse(localStorage.getItem(key));
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(
+      key,
+      JSON.stringify(Array.isArray(value) ? value.filter(filter) : value)
+    );
+  }, [key, filter, value]);
+
+  return [value, setValue];
+}
