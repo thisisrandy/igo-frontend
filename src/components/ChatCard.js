d8f08993d63144fabc775a363997c1dc771fc078
Randy West <randywest55@gmail.com>
2021-05-17 18:17:29 -0700

    Add ChatCard w/basic select and dispatch

---
+++ src/components/ChatCard.js
@@ -0,0 +1,35 @@
+import { send } from "@giantmachines/redux-websocket/dist";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { KEY, MESSAGE, TYPE } from "../constants/OutgoingMessageKeys";
+import { CHAT_MESSAGE } from "../constants/OutgoingMessageTypes";
+import { KEYS, YOUR_COLOR, CHAT_MESSAGES } from "../constants/StateKeys";
+import { useStyles } from "../hooks/useStyles";
+
+function ChatCard({ joinedToGame }) {
+  const classes = useStyles();
+  const {
+    [YOUR_COLOR]: yourColor,
+    [KEYS]: keys,
+    [CHAT_MESSAGES]: chatMessages,
+  } = useSelector((state) => state.game);
+  const dispatch = useDispatch();
+
+  const [message, setMessage] = useState("");
+  const sendMessageHandler = () => {
+    if (joinedToGame && message) {
+      dispatch(
+        send({
+          [TYPE]: CHAT_MESSAGE,
+          [KEY]: keys[yourColor],
+          [MESSAGE]: message,
+        })
+      );
+      setMessage("");
+    }
+  };
+
+  return null;
+}
+
+export default ChatCard;
