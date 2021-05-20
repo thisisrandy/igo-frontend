ada3ca167bd9e49e4d92adb6a1b7a36fc874d7bf
Randy West <randywest55@gmail.com>
2021-05-20 16:10:46 -0700

    Organize tests into describes

--- src/tests/utils.test.js
+++ src/tests/utils.test.js
@@ -1,13 +1,17 @@
 import { breakLongWords, capitalizeFirstLetter } from "../utils";
 
-test("capitalizes the first letter", () => {
-  expect(capitalizeFirstLetter("foo")).toBe("Foo");
+describe("test capitalizeFirstLetter", () => {
+  test("capitalizes the first letter", () => {
+    expect(capitalizeFirstLetter("foo")).toBe("Foo");
+  });
+
+  test("doesn't change other letters' capitalization", () => {
+    expect(capitalizeFirstLetter("FoO")).toBe("FoO");
+  });
 });
 
-test("doesn't change other letters' capitalization", () => {
-  expect(capitalizeFirstLetter("FoO")).toBe("FoO");
-});
-
-test("correctly breaks long words", () => {
-  expect(breakLongWords("hello world", 4)).toBe("hel- lo wor- ld");
+describe("test breakLongWords", () => {
+  test("correctly breaks long words", () => {
+    expect(breakLongWords("hello world", 4)).toBe("hel- lo wor- ld");
+  });
 });
