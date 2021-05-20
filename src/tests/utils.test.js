898633d623c96c3c7350fbbaa17331b16193cdac
Randy West <randywest55@gmail.com>
2021-05-20 16:11:10 -0700

    Add getDaysSince + tests

--- src/tests/utils.test.js
+++ src/tests/utils.test.js
@@ -1,4 +1,4 @@
-import { breakLongWords, capitalizeFirstLetter } from "../utils";
+import { breakLongWords, capitalizeFirstLetter, getDaysSince } from "../utils";
 
 describe("test capitalizeFirstLetter", () => {
   test("capitalizes the first letter", () => {
@@ -13,5 +13,49 @@ describe("test capitalizeFirstLetter", () => {
 describe("test breakLongWords", () => {
   test("correctly breaks long words", () => {
     expect(breakLongWords("hello world", 4)).toBe("hel- lo wor- ld");
+  });
+});
+
+describe("test daysSince", () => {
+  test("returns correct number of days having passed", () => {
+    expect(
+      getDaysSince(
+        new Date("1/1/20 11:59:00 PM"),
+        new Date("1/2/20 12:00:00 AM")
+      )
+    ).toBe(1);
+    expect(
+      getDaysSince(
+        new Date("1/1/20 11:59:00 PM"),
+        new Date("1/2/20 10:05:44 PM")
+      )
+    ).toBe(1);
+    expect(
+      getDaysSince(
+        new Date("1/1/20 11:59:00 AM"),
+        new Date("1/1/20 12:00:00 PM")
+      )
+    ).toBe(0);
+    expect(
+      getDaysSince(
+        new Date("11/1/20 1:00:00 PM"),
+        new Date("12/2/20 10:00:00 AM")
+      )
+    ).toBe(31);
+  });
+
+  test("works with millisecond inputs", () => {
+    expect(
+      getDaysSince(
+        new Date("1/1/20 11:59:00 PM").valueOf(),
+        new Date("1/2/20 12:00:00 AM").valueOf()
+      )
+    ).toBe(1);
+    expect(
+      getDaysSince(
+        new Date("11/1/20 1:00:00 PM").valueOf(),
+        new Date("12/2/20 10:00:00 AM").valueOf()
+      )
+    ).toBe(31);
   });
 });
--- src/utils/index.js
+++ src/utils/index.js
@@ -40,3 +40,23 @@ export function breakLongWords(text, lineWidth) {
 
   return res.join(" ");
 }
+
+/**
+ * Return the number of whole days that have passed between time and now. For
+ * example, if time corresponds to 1/1/20 11:59 PM and it is now 1/2/20 12:00AM,
+ * the return value is 1, because although only 1 minute has passed, the day (in
+ * local time) has changed
+ */
+export function getDaysSince(time, now = Date.now()) {
+  if (isNaN(time))
+    throw new TypeError(`time (${time}) is not a number or Date`);
+  if (isNaN(now)) throw new TypeError(`now (${now}) is not a number or Date`);
+
+  const midnight = (t) => {
+    t = t instanceof Date ? t : new Date(t);
+    return new Date(t.toDateString()).valueOf();
+  };
+
+  // 24 * 60 * 60 * 1000 = 86400000
+  return Math.floor((midnight(now) - midnight(time)) / 86400000);
+}
