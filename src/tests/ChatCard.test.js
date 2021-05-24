069c9deb1cbd0da4972a989cb85f0a51a61226d8
Randy West <randywest55@gmail.com>
2021-05-23 17:40:33 -0700

    Add chat snackbar tests

--- src/tests/ChatCard.test.js
+++ src/tests/ChatCard.test.js
@@ -3,16 +3,17 @@ import { YOUR_COLOR, KEYS, CHAT_MESSAGES } from "../constants/StateKeys";
 import createMockStore from "redux-mock-store";
 import { TYPE } from "../constants/OutgoingMessageKeys";
 import { CHAT_MESSAGE } from "../constants/OutgoingMessageTypes";
-import { render, act, screen } from "@testing-library/react";
+import { render, act, screen, within } from "@testing-library/react";
 import { Provider } from "react-redux";
 import ChatCard from "../components/ChatCard";
 import { ID, COLOR, MESSAGE, TIMESTAMP } from "../constants/ChatMessageKeys";
 import userEvent from "@testing-library/user-event";
+import { GAME } from "../constants/ReducerKeys";
 
 const keyW = "0123456789";
 const keyB = "9876543210";
 const state = {
-  game: {
+  [GAME]: {
     [YOUR_COLOR]: BLACK,
     [KEYS]: { [WHITE]: keyW, [BLACK]: keyB },
     [CHAT_MESSAGES]: [
@@ -89,4 +90,54 @@ test("send disabled when message is empty", () => {
     sendButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
   });
   expect(store.getActions().length).toBe(0);
+});
+
+test("snackbar displayed when latest message from opponent", () => {
+  const whiteSends = {
+    [GAME]: {
+      ...state[GAME],
+
+      [CHAT_MESSAGES]: state[GAME][CHAT_MESSAGES].concat({
+        [ID]: "1231231232",
+        [COLOR]: WHITE,
+        [TIMESTAMP]: Date.now() / 1000,
+        [MESSAGE]: "cheese for prime minister",
+      }),
+    },
+  };
+  render(
+    <Provider store={mockStore(whiteSends)}>
+      <ChatCard joinedToGame={true} />
+    </Provider>
+  );
+
+  const snackbar = screen.getByRole("alert");
+  expect(snackbar).toBeInTheDocument();
+  const message = within(snackbar).getByText(/^White says: cheese/);
+  expect(message).toBeInTheDocument();
+});
+
+test("snackbar not displayed when latest message from self", () => {
+  // NOTE: we're putting another message on the stack here to make sure that the
+  // timestamp is recent enough to display (even though it ultimately shouldn't)
+  const blackSends = {
+    [GAME]: {
+      ...state[GAME],
+
+      [CHAT_MESSAGES]: state[GAME][CHAT_MESSAGES].concat({
+        [ID]: "1231231232",
+        [COLOR]: BLACK,
+        [TIMESTAMP]: Date.now() / 1000,
+        [MESSAGE]: "cheese for prime minister",
+      }),
+    },
+  };
+  render(
+    <Provider store={mockStore(blackSends)}>
+      <ChatCard joinedToGame={true} />
+    </Provider>
+  );
+
+  const snackbar = screen.queryByRole("alert");
+  expect(snackbar).not.toBeInTheDocument();
 });
