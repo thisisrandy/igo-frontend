6a643e0c5a9e2400c49c357e95b7e5aeb873ae7d
Randy West <randywest55@gmail.com>
2021-06-19 15:42:14 -0700

    Add chat and opp conn'd incoming msg types

--- src/constants/IncomingMessageTypes.js
+++ src/constants/IncomingMessageTypes.js
@@ -2,3 +2,5 @@ export const NEW_GAME_RESPONSE = "new_game_response";
 export const JOIN_GAME_RESPONSE = "join_game_response";
 export const GAME_ACTION_RESPONSE = "game_action_response";
 export const GAME_STATUS = "game_status";
+export const CHAT = "chat";
+export const OPPONENT_CONNECTED = "opponent_connected";
