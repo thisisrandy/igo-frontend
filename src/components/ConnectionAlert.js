fcf1f8c9b66f4d9de23d1e3ef56bd445a91aa4ae
Randy West <randywest55@gmail.com>
2021-07-11 14:19:24 -0700

    Don't display server uri in conn alert
    Obviously it isn't a secret, but it doesn't really provide useful info
    to the average user, either

--- src/components/ConnectionAlert.js
+++ src/components/ConnectionAlert.js
@@ -4,11 +4,9 @@ import { CONNECTED } from "../constants/StateKeys";
 import { useSelector } from "react-redux";
 import { CONNECTION_ALERT_DELAY_MS } from "../constants/ConnectionSettings";
 import { GAME } from "../constants/ReducerKeys";
-import { breakLongWords } from "../utils";
 
 function ConnectionAlert({ zIndex }) {
   const { [CONNECTED]: connected } = useSelector((state) => state[GAME]);
-  const serverUri = process.env.REACT_APP_GAME_SERVER_URI;
 
   // We don't want this popping up immediately before the initial connection
   // under normal circumstances, so give it a small delay
@@ -27,13 +25,8 @@ function ConnectionAlert({ zIndex }) {
 
   return (
     <PersistentAlert zIndex={zIndex} open={!hidden && !connected}>
-      Connection to the game server (
-      {/* On small screens, a long websocket string can cause ugly horizontal
-          scrolling, so break it up if need be */}
-      {document.documentElement.clientWidth >= 500
-        ? serverUri
-        : breakLongWords(serverUri, 30)}
-      ) either lost or not previously established. Attempting to (re)connect...
+      Connection to the game server either lost or not previously established.
+      Attempting to (re)connect...
     </PersistentAlert>
   );
 }
